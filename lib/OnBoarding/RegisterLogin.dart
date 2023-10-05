a660916519fe6885d694f72d58584f00ec63b52b
“Carlos <carloscasta.perea@gmail.com>
2023-10-06 01:25:36 +0200

    RegisterView methods done

--- lib/OnBoarding/RegisterLogin.dart
+++ lib/OnBoarding/RegisterLogin.dart
@@ -1,6 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegisterLogin extends StatelessWidget{
+
+  //Constants
+  late BuildContext _context;
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final rePasswordController = TextEditingController();
+
+  //Methods
+
+  SnackBar snackBar = SnackBar(
+    content: Text('Contraseña incorrecta, inténtelo de nuevo'),
+  );
+
+  void onClickCancelar(){
+    Navigator.of(_context).pushNamed("/loginview");
+  }
+
+  void onClickAceptar() async {
+    if(passwordController.text==rePasswordController.text) {
+      try {
+
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: usernameController.text,
+          password: passwordController.text,
+        );
+
+        Navigator.of(_context).pushNamed("/perfilview");
+
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          print('The password provided is too weak.');
+        } else if (e.code == 'email-already-in-use') {
+          print('The account already exists for that email.');
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+    else{
+      ScaffoldMessenger.of(_context).showSnackBar(snackBar);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
