2354c31413c49a2776bcc46c036f21d51e486d84
“Carlos <carloscasta.perea@gmail.com>
2023-10-06 01:24:43 +0200

    RegisterView created

---
+++ lib/OnBoarding/RegisterLogin.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RegisterLogin extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+  }
+}
