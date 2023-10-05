f08054ffe1e7b5ed896e45e93a5e08b686563fa4
“Carlos <carloscasta.perea@gmail.com>
2023-10-05 19:54:17 +0200

    MaterialApp and route added to Kyty class

--- lib/Kyty.dart
+++ lib/Kyty.dart
@@ -4,6 +4,15 @@ class Kyty extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-  }
 
+    MaterialApp materialApp = MaterialApp(title: "Kyty",
+    routes: {
+      '/loginview' : (context) => LoginView(),
+    },
+      initialRoute: '/loginview',
+    );
+
+    return materialApp;
+
+  }
 }
