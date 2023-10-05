b12c5f462a4c30156285e0669fee49476040aadb
“Carlos <carloscasta.perea@gmail.com>
2023-10-05 22:55:44 +0200

    Personalized_TextFields created (TextFields with parameter) & LoginView with methods

--- lib/OnBoarding/LoginView.dart
+++ lib/OnBoarding/LoginView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kyty/Services/Personalized_TextFields.dart';
 
 class LoginView extends StatelessWidget{
   @override
@@ -18,6 +19,19 @@ class LoginView extends StatelessWidget{
       Text('¡Bienvenido de nuevo!',
         style: TextStyle(color: Colors.grey[700],
         fontSize: 16,),),
+
+      const SizedBox(height: 25,),
+
+      //username TextField
+
+      Personalized_TextFields(),
+
+      const SizedBox(height: 10,),
+
+      //password TextField
+
+      Personalized_TextFields(),
+
     ],);
 
     Scaffold scaf = Scaffold(backgroundColor: Colors.grey[300],
---
+++ lib/Services/Personalized_TextFields.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Personalized_TextFields extends StatelessWidget{
+
+  final controller;
+  final String hintText;
+  final bool obscuredText;
+
+  const Personalized_TextFields ({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscuredText,
+
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscuredText,
+        decoration: InputDecoration(
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade400),
+          ),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          hintText: hintText,
+        ),
+      ),
+    );
+
+  }
+}
