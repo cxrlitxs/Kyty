00458a78f8ca602fff4d06de0477d59e5c0056ef
“Carlos <carloscasta.perea@gmail.com>
2023-10-07 18:52:05 +0200

    FbUser created and done

---
+++ lib/FbClasses/FbUser.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FbUser{
+
+  final String firstName;
+  final String lastName;
+  final int age;
+  final double height;
+
+  FbUser ({
+    required this.firstName,
+    required this.lastName,
+    required this.age,
+    required this.height
+  });
+
+  factory FbUser.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options,
+      ) {
+    final data = snapshot.data();
+    return FbFbUser(
+        firstName: data?['firstName'],
+        lastName: data?['lastName'],
+        age: data?['age'],
+        height:data?['height']
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      if (firstName != null) "firstName": firstName,
+      if (lastName != null) "lastName": lastName,
+      if (age != null) "age": age,
+      if (height != null) "height": height,
+    };
+  }
+
+}
