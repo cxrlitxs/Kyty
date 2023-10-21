af436bfa54fd1b7fa00f55d7d7a8c5d49fa48909
“Carlos <carloscasta.perea@gmail.com>
2023-10-21 03:00:06 +0200

    FbPost.dart Created

--- lib/FbClasses/FbPost.dart
+++ lib/FbClasses/FbPost.dart
@@ -20,14 +20,49 @@ class FbPost{
     return FbPost(
         title: data?['title'],
         body: data?['body'],
-        date: Timestamp.now(),
+        date: data?['date'],
     );
   }
 
   Map<String, dynamic> toFirestore() {
     return {
       if (title != null) "titulo": title,
-      if (body != null) "cuerpo": body
+      if (body != null) "cuerpo": body,
+      if (date != null) "date": Timestamp.now(),
     };
+  }
+
+  String formattedData(){
+    // Timestamp to DateTime object
+    DateTime dateTime = date.toDate();
+
+    // Get the hour and the minute.
+    int hour = dateTime.hour;
+    int minute = dateTime.minute;
+
+    // Its AM o PM?.
+    String amPm = hour < 12 ? 'AM' : 'PM';
+
+    // Adjust if its PM.
+    if (hour > 12) {
+      hour -= 12;
+    }
+
+    // Format in a readable format.
+    String formattedHour = hour.toString().padLeft(2, '0');
+    String formattedMinute = minute.toString().padLeft(2, '0');
+
+    // The hour, minutes, and AM/PM into a text string.
+    return  '${dateTime.day}'
+            '/${dateTime.month}'
+            '/${dateTime.year}'
+            '   $formattedHour:$formattedMinute $amPm';
+
+
+    //return '${dateTime.day}'
+    //         '/${dateTime.month}'
+    //         '/${dateTime.year}'
+    //         '  ${dateTime.hour}'
+    //         ':${dateTime.minute}';
   }
 }
