3cfdf1403e6bbde26122d70ef671eff4a069fa11
“Carlos <carloscasta.perea@gmail.com>
2023-10-21 03:31:33 +0200

    PostGridCellView.dart Created

---
+++ lib/Services/PostGridCellView.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PostGridCellView extends StatelessWidget{
+
+  final String sText;
+  final String sBody;
+  final String sDate;
+  final int iColorCode;
+  final double dFontSize;
+
+  const PostGridCellView({super.key,
+    required this.sText,
+    required this.sBody,
+    required this.sDate,
+    required this.iColorCode,
+    required this.dFontSize
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return FractionallySizedBox(
+        child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8)),
+            margin: const EdgeInsets.only(top: 25, left: 25, right: 25, bottom: 25),
+            padding: EdgeInsets.all(25),
+            //color: Colors.amber[iColorCode],
+            child: Row(
+              children: [
+                Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(sText,style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+                        const SizedBox(height: 10,),
+                        Text(sBody),
+                        const SizedBox(height: 10,),
+                        Text(sDate),
+                      ],
+                    )
+                )
+              ],
+            )
+        ),
+      );
+  }
+}
