8a35ea90a89b69e6c46ca56d1c85f3a7a4bdab9b
“Carlos <carloscasta.perea@gmail.com>
2023-11-18 19:21:33 +0100

    PlatformAdmin created

---
+++ lib/Singletone/PlatformAdmin.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PlatformAdmin{
+
+  BuildContext context;
+
+  PlatformAdmin({required this.context});
+
+  double getScreenWidth(){
+    double width = MediaQuery.of(context).size.width;
+    return width;
+  }
+
+  double getScreenHeight(){
+    double height = MediaQuery.of(context).size.height;
+    return height;
+  }
+
+
+}
